60e9c9976f96715bd64c39ea173628481bc59077
Killer2407 <munimaniket0797@gmail.com>
2023-02-15 13:50:52 -0500

    hotel

--- client/src/component/searchItem/searchItem.jsx
+++ client/src/component/searchItem/searchItem.jsx
@@ -4,17 +4,27 @@ import './searchItem.css'
 const SearchItem = () => {
   return (
     <div className="searchItem">
-        <img src="https://thumbs.dreamstime.com/b/luxury-hotel-4480742.jpg" alt="" className="searchImage" />
-        <div className="searchDesc"> 
+      <img src="https://thumbs.dreamstime.com/b/luxury-hotel-4480742.jpg" alt="" className="searchImage" />
+      <div className="searchDesc">
         <h1 className="searchTitle">Tower street apartment</h1>
-                <span className="searchDistance">500m from center</span>
-                <span className="searchTaxi">Free Airport Taxi</span>
-                <span className="searchSubtitle">Studio Apartment with Air Conditioning</span>
-                <span className="searchFeatures">Entire Studio * 1 bathroom * 21m 1 full bed</span>
-                <span className="searchCancel">Free cancellation</span>
-                <span className="searchCancelSubtitle">You can cancel, so lock in this great price today!</span>
+        <span className="searchDistance">500m from center</span>
+        <span className="searchTaxi">Free Airport Taxi</span>
+        <span className="searchSubtitle">Studio Apartment with Air Conditioning</span>
+        <span className="searchFeatures">Entire Studio * 1 bathroom * 21m 1 full bed</span>
+        <span className="searchCancel">Free cancellation</span>
+        <span className="searchCancelSubtitle">You can cancel, so lock in this great price today!</span>
+      </div>
+      <div className="searchDetails">
+        <div className="searchRating">
+          <span> Excellent </span>
+          <button> 9.2 </button>
         </div>
-        <div className="searchDetails"></div>
+        <div className="searchDetailsText">
+          <span className='searchPrice'> $121 </span>
+          <span className='searchTax'> Includes taxes and fees </span>
+          <button className='searchCheckbutton'>See Availability</button>
+        </div>
+      </div>
     </div>
   )
 }
